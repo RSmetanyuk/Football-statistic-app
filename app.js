2cf00f6773a2d4874594125e8f0211c70e2aca44
1 <RSmetanyuk>
2017-09-27 21:52:21 +0300

    Problems with routing

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = angular.module('main', []);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/championships', {
+      templateUrl: 'templates/championships.template.html',
+      controller: 'championshipscontroler'
+    })
+    .when('/teams', {
+      templateUrl: 'templates/teams.template.html',
+      controller: 'teamscontroler'
+    });
+});
+
+app.controller('championshipscontroler', function($scope, $http) {
+  $http.get("https://footballbet.com.ua/api/championships/")
+  .then(function(response) {
+      $scope.countries = response.data.result;
+  });
+});
+
+
+app.controller('teamscontroler', function($scope, $http) {
+  $http.get("https://footballbet.com.ua/api/teams/")
+  .then(function(response) {
+      $scope.teams = response.data.result;
+  });
+});
--- module.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('main', []);
-app.controller('championships', function($scope, $http) {
-  $http.get("https://footballbet.com.ua/api/championships/")
-  .then(function(response) {
-      $scope.countries = response.data.result;
-  });
-});
