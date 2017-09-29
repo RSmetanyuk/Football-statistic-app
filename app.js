e1264c6c8fdf1c85dfa6fae30d89109d74eaac11
RSmetanyuk <rsmetanyuk@gmail.com>
2017-09-30 02:29:17 +0300

    #6. Added pagination

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('main', ["ngRoute"]);
+var app = angular.module('main', ["ngRoute", "angularUtils.directives.dirPagination"]);
 
 app.config(function($routeProvider){
   $routeProvider
