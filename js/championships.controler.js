7735c3aaaf4d51cff1dde11d52281303eed3a8f6
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-02 18:00:12 +0300

    #13. Added ability to navigate from match to championship

--- js/championships.controler.js
+++ js/championships.controler.js
@@ -1,7 +1,12 @@
 app.controller('championshipscontroler', function($scope, $http, $routeParams) {
   $http.get("https://footballbet.com.ua/api/championships/")
   .then(function(response) {
-      $scope.countries = response.data.result;      
+    $scope.countries = response.data.result; 
+    $scope.idChampionship = $routeParams.id_championship;
+    if(!isFinite($scope.idChampionship)) {
+      for (var i = 0; i < $scope.countries.length; i++) {
+        if ($routeParams.id_championship.indexOf($scope.countries[i].name) >= 0) {$scope.idChampionship = $scope.countries[i].id_championship;};
+      }    
+    };     
   });
-  $scope.idChampionship = $routeParams.id_championship;
 });
--- js/teams.controler.js
+++ js/teams.controler.js
@@ -2,7 +2,7 @@ app.controller('teamscontroler', function($scope, $http, $routeParams) {
   $http.get("https://footballbet.com.ua/api/teams/")
   .then(function(response) {
       $scope.teams = response.data.result;
-      for (i = 0; i < $scope.teams.length; i++) {
+      for (var i = 0; i < $scope.teams.length; i++) {
         if ($scope.teams[i].id_teams === $routeParams.team_number) $scope.teamNumber = i;
       };
   });
