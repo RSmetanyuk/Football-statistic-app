e96e1e5218300f142ab485a3f12b82f3a0122b98
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-02 12:35:55 +0300

    #9. Added active link highlighting

--- js/championships.controler.js
+++ js/championships.controler.js
@@ -1,7 +1,7 @@
 app.controller('championshipscontroler', function($scope, $http, $routeParams) {
   $http.get("https://footballbet.com.ua/api/championships/")
   .then(function(response) {
-      $scope.countries = response.data.result;
-      $scope.idChampionship = $routeParams.id_championship;
+      $scope.countries = response.data.result;      
   });
+  $scope.idChampionship = $routeParams.id_championship;
 });
