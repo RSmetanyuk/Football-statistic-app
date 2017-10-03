7368a625645b5faea01a4a66e93ba9867182aba3
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-03 16:58:48 +0300

    #13. Routing from matches to championship by id done

--- js/match.details.controler.js
+++ js/match.details.controler.js
@@ -1,4 +1,11 @@
 app.controller('matchDetailsControler', function($scope, $routeParams, DataFactory) {
   $scope.Data = DataFactory;
   $scope.idMatch = $routeParams.id_match;
+  for (var i = 0; i < $scope.Data.matches.length; i++) {
+    for (var j = 0; j < $scope.Data.countries.length; j++) {
+      if ($scope.Data.matches[i].title.indexOf($scope.Data.countries[j].name) >= 0) {
+        $scope.Data.matches[i].id_championship = $scope.Data.countries[j].id_championship;
+      };
+    };
+  };  
 });
