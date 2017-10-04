4ca5c01f7ccd8efef643e1e3694eab0305ec3f63
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-04 23:24:21 +0300

    #22. App enhancements

--- js/match.details.controler.js
+++ js/match.details.controler.js
@@ -4,7 +4,7 @@ app.controller('matchDetailsControler', function($scope, $routeParams, DataFacto
   $scope.Data.getApi("championships");
   $scope.Data.getApi("matches");
   $scope.Data.getApi("matches");
-  for (var i = 0; i < $scope.Data.matches.length; i++) {
+  for (var i = 0; i < 100; i++) {
     for (var j = 0; j < $scope.Data.championships.length; j++) {
       if ($scope.Data.matches[i].title.indexOf($scope.Data.championships[j].name) >= 0) {
         $scope.Data.matches[i].id_championship = $scope.Data.championships[j].id_championship;
