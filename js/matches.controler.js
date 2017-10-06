76a18e003d7fbdd08aba4f9a5e70965d99d2ce0e
RSmetanyuk <rsmetanyuk@gmail.com>
2017-10-06 23:11:13 +0300

    matches: search fixed

--- js/matches.controler.js
+++ js/matches.controler.js
@@ -1,5 +1,9 @@
 app.controller('matchesControler', function($scope, DataFactory) {
   $scope.Data = DataFactory;
-  $scope.Data.getApi("matches");
   $scope.searchActive = false;
+  $scope.Data.getApi("matches").then(function() {
+    for (var i = 0; i < $scope.Data.matches.length; i++) {
+      $scope.Data.matches[i].teamPair = $scope.Data.matches[i].firstTeam.concat(" - ", $scope.Data.matches[i].secondTeam);    
+    };
+  });
 });
